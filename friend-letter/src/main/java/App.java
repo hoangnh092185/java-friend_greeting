2ae54c8abe7d3583e200da49832420d30c91b787
Nhat Hoang <hoangnh092185@gmail.com>
2016-09-12 20:36:24 -0700

    add bdd page.

---
+++ friend-letter/src/main/java/App.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/greeting_card", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  String recipient = request.queryParams("recipient");
+  String sender = request.queryParams("sender");
+  model.put("recipient", recipient);
+  model.put("sender", sender);
+  model.put("template", "templates/greeting_card.vtl");
+  return new ModelAndView(model, "templates/layout.vtl");
+}, new VelocityTemplateEngine());
+
+    get("/form", (request, response)-> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("username", request.session().attribute("username"));
+      model.put("template", "templates/welcome.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/favorite_photos", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/welcome", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+
+  String inputtedUsername = request.queryParams("username");
+  request.session().attribute("username", inputtedUsername);
+  model.put("username", inputtedUsername);
+
+  model.put("template", "templates/welcome.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+  }
+
+}
